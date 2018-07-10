b029e406bca066725d373c989d8ffb1b49bb7764
Cece Zinny <cwzinny@gmail.com>
2018-07-10 15:55:29 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,10 +1,10 @@
 # Build your say_hello method here
 
-def say_hello
-  your_name = "Cece"
+def say_hello(your_name)
+  # your_name = "Cece"
   puts "Hello #{your_name}!"
 end
 
-say_hello
+say_hello("cece")
 
 # say_hello("Gabriella")
